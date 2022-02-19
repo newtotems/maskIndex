c18eb644f2f959a94e9ffbd0d5a266577d7c1ec0
newtotems <71979980+newtotems@users.noreply.github.com>
2022-02-19 14:39:20 +0000

    .

---
+++ _data/products.js
@@ -0,0 +1,19 @@
+const faunadb = require('faunadb'),
+  q = faunadb.query;
+
+module.exports = async function() {
+	const client = new faunadb.Client({ secret: process.env.FAUNADB })
+
+	let productObs = await client.query(
+		q.Map(
+			q.Paginate(q.Documents(q.Collection('masks'))),
+			q.Lambda(x => q.Get(x))
+		)
+	);
+	let data = productObs.data.map(po => {
+		return po.data;
+	});
+	console.log(data.length + ' products loaded from Fauna');
+	return data;
+
+}
